5c4a5cf8417864f485196d5ed38d4bf5b6280b78
syjeon <syjeon@ntels.com>
2020-07-27 10:20:21 +0900

    주석

--- src/main/java/com/ntels/syjeon/covid19/controller/CovidController.java
+++ src/main/java/com/ntels/syjeon/covid19/controller/CovidController.java
@@ -27,29 +27,51 @@ public class CovidController {
     @Autowired
     private CovidService covidService;
 
+    /**
+     * 초기 view Load
+     * @return
+     */
     @GetMapping(value = "/")
-    public String  index() {
+    public String index() {
         return "covid";
     }
 
+    /**
+     * 시 / 도 리스트 반환컨트롤러
+     * @return
+     */
     @GetMapping(value = "/names.ajax")
     @ResponseBody
     public String[] getNameList(){
         return covidService.getNames();
     }
 
+    /**
+     * 시 / 도 History 반환 컨트롤러
+     * @param gubun
+     * @return List<Item>
+     */
     @GetMapping(value = "/history.ajax")
     @ResponseBody
     public List<Item> getNameList(String gubun){
         return covidService.getHistory(gubun);
     }
 
+    /**
+     * Data Refresh 처리 컨트롤러
+     * @return boolean
+     */
     @GetMapping(value = "/refresh.ajax")
     @ResponseBody
     public boolean refrash(){
         return covidService.getApiList();
     }
 
+    /**
+     * 시 / 도 상세 현황 조회 컨트롤러
+     * @param gubun
+     * @return View and Item
+     */
     @GetMapping(value = "/{gubun}")
     public ModelAndView view(@PathVariable(value = "gubun") String gubun) {
         ModelAndView mv = new ModelAndView("covid");
